6ba554b18d51c52fce1480caaff206b3cbdb46f3
Eisha215 <eishasajid01@gmail.com>
2023-08-02 10:39:35 +0500

    chore:minor tweaks

---
+++ src/index.js
@@ -0,0 +1,120 @@
+let nav = document.getElementById('navMenu');
+            function toggle(){
+                nav.classList.toggle('hidden')
+            }
+
+var canvasElement = document.querySelector("#myCanvas1");
+    var canvasElement2 = document.querySelector("#myCanvas2");
+    var canvasElement3 = document.getElementById("myCanvas3");
+    var canvasElement4 = document.querySelector("#myCanvas4");
+    var canvasElement5 = document.querySelector("#myCanvas5");
+    var canvasElement6 = document.querySelector("#myCanvas6");
+    var canvasElement7 = document.querySelector("#myCanvas7");
+
+
+
+
+        
+    var context = canvasElement.getContext("2d");
+    var context2 = canvasElement2.getContext("2d");
+    var context3 =  canvasElement3.getContext("2d");
+    var context4 = canvasElement4.getContext("2d");
+    var context5 = canvasElement5.getContext("2d");
+    var context6 = canvasElement6.getContext("2d");
+    var context7 = canvasElement7.getContext("2d");
+
+
+
+
+    context.beginPath();
+    context.moveTo(100, 100);
+    context.lineTo(100, 300);
+    context.lineTo(300, 300);
+    context.closePath();
+
+    context.lineWidth = 1;
+    context.strokeStyle = '#666666';
+    context.stroke();
+
+    context.fillStyle = "#b25a5a";
+    context.fill();
+
+    context2.beginPath();
+    context2.moveTo(100, 100);
+    context2.lineTo(100, 300);
+    context2.lineTo(300, 300);
+    context2.closePath();
+
+    context2.lineWidth = 1;
+    context2.strokeStyle = '#666666';
+    context2.stroke();
+
+    context2.fillStyle = "#b25a5a";
+    context2.fill();
+
+    context3.beginPath();
+    context3.moveTo(100, 100);
+    context3.lineTo(100, 300);
+    context3.lineTo(300, 300);
+    context3.closePath();
+
+    context3.lineWidth = 1;
+    context3.strokeStyle = '#666666';
+    context3.stroke();
+
+    context3.fillStyle = "#b25a5a";
+    context3.fill();
+
+    context4.beginPath();
+    context4.moveTo(100, 100);
+    context4.lineTo(100, 300);
+    context4.lineTo(300, 300);
+    context4.closePath();
+
+    context4.lineWidth = 1;
+    context4.strokeStyle = '#666666';
+    context4.stroke();
+
+    context4.fillStyle = "#b25a5a";
+    context4.fill();
+
+    context5.beginPath();
+    context5.moveTo(100,0);
+    context5.lineTo(100, 100);
+    context5.lineTo(300, 0);
+    context5.closePath();
+
+    context5.lineWidth = 1;
+    context5.strokeStyle = '#666666';
+    context5.stroke();
+
+    context5.fillStyle = "#b25a5a";
+    context5.fill();
+
+    
+    context6.beginPath();
+    context6.moveTo(100,0);
+    context6.lineTo(100, 100);
+    context6.lineTo(300, 0);
+    context6.closePath();
+
+    context6.lineWidth = 1;
+    context6.strokeStyle = '#666666';
+    context6.stroke();
+
+    context6.fillStyle = "#b25a5a";
+    context6.fill();
+
+    
+    context7.beginPath();
+    context7.moveTo(100,0);
+    context7.lineTo(100, 100);
+    context7.lineTo(300, 0);
+    context7.closePath();
+
+    context7.lineWidth = 1;
+    context7.strokeStyle = '#666666';
+    context7.stroke();
+
+    context7.fillStyle = "#b25a5a";
+    context7.fill();
